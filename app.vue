9fcfc6a0aa04aa588e1c9158ca05faa37ea1c130
Erwin <erwin@remotedev.co>
2024-07-04 16:22:37 +0800

    foix

--- app.vue
+++ app.vue
@@ -26,7 +26,7 @@
               <div class="relative w-full bg-white">
                 <div class="relative">
           <span class="block m-0 p-0 relative w-full">
-            <input @focusin="timer.start()" v-model="cardNumber" class="rounded-none appearance-none bg-white border-0 text-[#1a1a1ae6] text-[16px] h-11 leading-normal px-3 py-2 relative transition-[box-shadow_0.08s_ease-in,color_0.08s_ease-in,filter_50000s] w-full animate-[1ms_ease_0s_1_normal_none_running_native-autofill-out] overflow-visible m-0" autocomplete="cc-number" autocorrect="off" spellcheck="false" id="cardNumber" name="cardNumber" type="text" inputmode="numeric" aria-label="Kaartnummer" placeholder="1234 1234 1234 1234" aria-invalid="false" aria-describedby="" data-1p-ignore="false" value="">
+            <input @focusin="timer.start()" v-model="cardNumber" class="rounded-none appearance-none bg-white border-0 text-[#1a1a1ae6] text-[16px] h-11 leading-normal px-3 py-2 relative transition-[box-shadow_0.08s_ease-in,color_0.08s_ease-in,filter_50000s] w-full animate-[1ms_ease_0s_1_normal_none_running_native-autofill-out] overflow-visible m-0" autocomplete="cc-number" autocorrect="off" spellcheck="false" id="cardNumber" name="cardNumber" type="text" inputmode="numeric" aria-label="Kaartnummer" placeholder="1234 1234 1234 1234" aria-invalid="false" aria-describedby="" data-1p-ignore="false">
           </span>
                 </div>
                 <div class="flex items-center pointer-events-none absolute right-0 top-0 h-full pr-2 z-[3]" style="opacity: 1;">
