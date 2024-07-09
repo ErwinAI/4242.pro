9e78b5057020e9f598828cc710556e93e41ae380
Erwin <erwin@remotedev.co>
2024-07-09 15:37:15 +0800

    remove https/www from share link

--- pages/index.vue
+++ pages/index.vue
@@ -390,7 +390,7 @@ class Timer {
               <button @click="restartGame()" v-if="hasPlayedGame" class="mt-20 flex mx-auto bg-white text-indigo-600 rounded-lg px-4 py-2">Play again</button>
 
               <template v-if="!timer.hasBeenActivated() || timer.isRunning() || (timer.hasStopped() && inputDeclaredValid)">
-                <p v-if="shareShortCode" class="mt-8 text-sm italic text-center break-all">Here is your shareable link (click to copy): <span class="hover:underline cursor-pointer" :class="hasCopiedShareLink ? 'text-green-200' : ''" @click="copyShareLink('https://www.4242.pro/s/' + shareShortCode)">https://www.4242.pro/s/{{ shareShortCode }} {{ hasCopiedShareLink ? '✅' : '' }}</span></p>
+                <p v-if="shareShortCode" class="mt-8 text-sm italic text-center break-all">Here is your shareable link (click to copy): <span class="hover:underline cursor-pointer" :class="hasCopiedShareLink ? 'text-green-200' : ''" @click="copyShareLink('4242.pro/s/' + shareShortCode)">4242.pro/s/{{ shareShortCode }} {{ hasCopiedShareLink ? '✅' : '' }}</span></p>
                 <p v-if="!shareShortCode && showConcludingMessage" class="text-white text-lg my-4 text-center font-bold">OR</p>
                 <div v-if="!shareShortCode && showConcludingMessage" class="flex mt-4 gap-x-2 justify-center">
                   <input type="text" v-model="inputScoreName" class="flex h-8 max-w-lg rounded-md border px-2 py-1 bg-white text-sm text-[#1a1a1ae6] leading-normal focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
