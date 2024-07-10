4ad2cd3b3d803331345925dacd970b1f1455120f
Erwin <erwin@remotedev.co>
2024-07-10 18:15:47 +0800

    don't decrypt anymore cause it gives error now with csrf

--- pages/s/[code].vue
+++ pages/s/[code].vue
@@ -5,37 +5,17 @@
 </template>
 
 <script setup>
-import { useRoute, useRouter } from '#app';
-import { ref } from 'vue';
+import { useRouter } from '#app';
 
-const route = useRoute();
 const router = useRouter();
-const decryptedCode = ref(null);
+defineOgImageComponent('Frame', {
+  title: '4242.pro - R u 10x dev?',
+  bg: 'linear-gradient(to bottom right, #4f46e5, #1e1b4b)'
+});
 
-const { data, error } = await useAsyncData('decryptedCode', () =>
-    $fetch('/api/decrypt', {
-      method: 'POST',
-      body: { code: route.params.code },
-    })
-);
+// Redirecting user with Nuxt
+await router.push('/')
 
-if (error.value) {
-  console.error('Error fetching decrypted code:', error.value);
-  decryptedCode.value = null;
-} else {
-  decryptedCode.value = data.value.decryptedCode;
-
-  const mode = decryptedCode.value.mode === 'full' ? ' the full checkout form ' : ' the creditcard form ';
-
-  defineOgImageComponent('Frame', {
-    description: decryptedCode.value.name + ' has completed ' + mode + ' in ' + decryptedCode.value.score.toFixed(4) + ' sec.',
-    title: '4242.pro - R u 10x dev?',
-    bg: 'linear-gradient(to bottom right, #4f46e5, #1e1b4b)'
-  });
-
-  // Redirecting user with Nuxt
-  await router.push('/');
-}
 </script>
 
 <style scoped>
