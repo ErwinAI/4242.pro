6a7d234451eb3b8f361e522c1d34c33291f548a6
Dom So <sobe.dominik@gmail.com>
2024-07-10 14:25:29 +0800

    add og image and update meta

--- app.vue
+++ app.vue
@@ -10,18 +10,22 @@ useHead({
     lang: 'en',
   },
   charset: 'utf-8',
+  title: '4242.pro - R u 10x engineer?',
+
   meta: [
     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-    { property: 'og:title', content: "4242.pro - R u 10x engineer?" },
+    { name: 'title', content: '4242.pro - R u 10x engineer?' },
+    { name: 'description', content: "Let's see if ur a 10x engineer or a n00b, your goal is to fill this checkout form asap." },
+    { property: 'og:title', content: '4242.pro - R u 10x engineer?' },
     { property: 'og:description', content: "Let's see if ur a 10x engineer or a n00b, your goal is to fill this checkout form asap." },
-    { property: 'og:type', content: "website" },
-    { property: 'og:url', content: "https://4242.pro/" },
-    { property: 'og:image:type', content: "image/png" },
-    { property: 'og:image:width', content: "1200" },
-    { property: 'og:image:height', content: "630" },
-    { property: 'og:image:alt', content: "4242.pro" },
-    { property: 'twitter:card', content: "summary_large_image" },
-    { property: 'twitter:site', content: "@erwin_ai" }
+    { property: 'og:type', content: 'website' },
+    { property: 'og:url', content: 'https://4242.pro/' },
+    { property: 'og:image:type', content: 'image/png' },
+    { property: 'og:image:width', content: '1200' },
+    { property: 'og:image:height', content: '630' },
+    { property: 'og:image:alt', content: '4242.pro' },
+    { property: 'twitter:card', content: 'summary_large_image' },
+    { property: 'twitter:site', content: '@erwin_ai' },
   ],
   link: [
     { rel: 'canonical', href: 'https://4242.pro/' },
@@ -30,10 +34,10 @@ useHead({
     { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
   ],
   script: [
-    {src: '/main.js'},
-    {src: 'https://scripts.simpleanalyticscdn.com/latest.js', body: true, async: true, defer: true, "data-collect-dnt": "true"}
-  ]
-});
+    { src: '/main.js' },
+    { src: 'https://scripts.simpleanalyticscdn.com/latest.js', body: true, async: true, defer: true, 'data-collect-dnt': 'true' },
+  ],
+})
 </script>
 
 <style>
